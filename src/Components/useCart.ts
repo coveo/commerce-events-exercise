5225b2519412cdda2a173388492264e62a677eb0
Sami Sayegh <ssayegh@coveo.com>
2023-05-18 15:19:24 -0400

    add removeAll

--- src/Components/useCart.ts
+++ src/Components/useCart.ts
@@ -39,9 +39,14 @@ export function useCart() {
     set(newItems)
   }
 
+  function removeAll() {
+    set([])
+  }
+
   return {
     addProduct,
     removeProduct,
+    removeAll,
     subscribe,
   }
 }
