f43098cb4a4a92f90c183e02aaff59221186da90
Sami Sayegh <ssayegh@coveo.com>
2023-05-19 15:35:56 -0400

    rename functions

--- src/Components/ScenarioButton.tsx
+++ src/Components/ScenarioButton.tsx
@@ -4,27 +4,26 @@ import { ScenarioResultModal } from "./ScenarioResultModal";
 
 export function ScenarioButton() {
   const [showModal, setShowModal] = useState(false);
-
   const { run } = useScenario();
 
   async function runScenario() {
     await run();
-    show();
+    showResults();
   }
 
-  function show() {
+  function showResults() {
     setShowModal(true);
   }
 
-  function hide() {
+  function hideResults() {
     setShowModal(false);
   }
 
   return (
     <div>
       <button onClick={runScenario}>Run scenario</button>
-      <button onClick={show}>Show results</button>
-      {showModal && <ScenarioResultModal hideModal={hide} />}
+      <button onClick={showResults}>Show results</button>
+      {showModal && <ScenarioResultModal hideModal={hideResults} />}
     </div>
   );
 }
