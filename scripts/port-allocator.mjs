92bad68d1b7cfa3618feb26e89f439b02598dca9
Sami Sayegh <ssayegh@coveo.com>
2023-05-23 17:49:22 -0400

    adjust port-allocator to work with codesandbox

--- scripts/port-allocator.mjs
+++ scripts/port-allocator.mjs
@@ -1,31 +1,45 @@
-import {appendFileSync, truncateSync, existsSync} from 'node:fs';
-import {EOL} from 'node:os';
-import getPort, {makeRange} from 'get-port';
-import {config} from 'dotenv';
-const environment = config();
+import {
+  appendFileSync,
+  truncateSync,
+  existsSync,
+  writeFileSync,
+} from "node:fs";
+import { EOL } from "node:os";
+import getPort, { makeRange } from "get-port";
+import { config } from "dotenv";
+config();
 
 const preferedWebAppPort = 3000;
 const portRangeFallback = [3000, 3999];
 
 const ensureEnvironmentFile = () => {
-  if (!existsSync('.env')) {
-    throw new Error(
-      '.env file not found in the project root. Refer to the README to for more information for more information.'
-    );
+  if (!existsSync(".env")) {
+    writeFileSync(".env", "");
   }
 };
 
 const updateEnvFile = (applicationPort, serverPort) => {
   const env = {
-    ...environment.parsed,
+    ...getDefaultEnvVariables(),
     PORT: applicationPort,
     REACT_APP_SERVER_PORT: serverPort,
   };
 
-  truncateSync('.env');
+  truncateSync(".env");
   for (const [key, value] of Object.entries(env)) {
-    appendFileSync('.env', `${key}=${value}${EOL}`);
+    appendFileSync(".env", `${key}=${value}${EOL}`);
   }
+};
+
+const getDefaultEnvVariables = () => {
+  return {
+    REACT_APP_PLATFORM_URL: process.env["REACT_APP_PLATFORM_URL"],
+    REACT_APP_ORGANIZATION_ID: process.env["REACT_APP_ORGANIZATION_ID"],
+    REACT_APP_API_KEY: process.env["REACT_APP_API_KEY"],
+    REACT_APP_USER_EMAIL: process.env["REACT_APP_USER_EMAIL"],
+    PORT: process.env["PORT"],
+    REACT_APP_SERVER_PORT: process.env["REACT_APP_SERVER_PORT"],
+  };
 };
 
 const allocatePorts = async () => {
@@ -39,7 +53,7 @@ const allocatePorts = async () => {
 };
 
 const getNextAvailablePorts = async (preferedPort) => {
-  if (typeof preferedPort !== 'number') {
+  if (typeof preferedPort !== "number") {
     preferedPort = isNaN(parseInt(preferedPort))
       ? null
       : parseInt(preferedPort);
@@ -48,11 +62,11 @@ const getNextAvailablePorts = async (preferedPort) => {
   if (await isPortAvailable(preferedPort)) {
     return preferedPort;
   }
-  return await getPort({port: makeRange(...portRangeFallback)});
+  return await getPort({ port: makeRange(...portRangeFallback) });
 };
 
 const isPortAvailable = async (port) => {
-  const availablePort = await getPort({port});
+  const availablePort = await getPort({ port });
   return availablePort === port;
 };
 
