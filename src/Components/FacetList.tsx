91e886530c50629a5f13966879082d9e8a187bde
Sami Sayegh <ssayegh@coveo.com>
2023-05-24 16:55:14 -0400

    change author facet to brand

--- src/Components/FacetList.tsx
+++ src/Components/FacetList.tsx
@@ -7,9 +7,9 @@ const FacetList = () => {
     <Box>
       <Box px={1} pb={1}>
         <Typography variant="overline">Refine By</Typography>
+        <Facet field="ec_brand" title="Brand" />
         <Facet field="objecttype" title="Object Type" />
         <Facet field="filetype" title="File Type" />
-        <Facet field="author" title="Author" />
       </Box>
     </Box>
   );
